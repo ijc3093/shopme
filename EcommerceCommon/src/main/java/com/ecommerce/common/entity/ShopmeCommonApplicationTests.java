8fbe1527c216283fa45f43279c84d4c97f2f2625
ijc3093 <49662848+ijc3093@users.noreply.github.com>
2022-08-15 15:28:58 -0400

    Replaced with Ecommerce

---
+++ EcommerceCommon/src/main/java/com/ecommerce/common/entity/ShopmeCommonApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ecommerce.common.entity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class ShopmeCommonApplicationTests {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopmeCommonApplicationTests.class, args);
+	}
+
+}
---
+++ EcommerceWebParent/EcommerceBackEnd/src/main/java/com/ecommerce/admin/user/UserNotFoundException.java
@@ -0,0 +1,11 @@
+package com.ecommerce.admin.user;
+
+public class UserNotFoundException extends Exception {
+
+	public UserNotFoundException(String message) {
+		super(message);
+	}
+
+	
+
+}
